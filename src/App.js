bfa197ce4b844bc671e5bf307b76022634bd5f9a
PKTseng <jk0910948659@gmail.com>
2023-06-11 12:48:22 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
 function App() {
+  const [count, addCount] = useState(0)
+  const [name, setName] = useState('ken')
+
+  console.log(count)
+
+  useEffect(() => {
+    document.title = `${count}`
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>{count}</h1>
+      <h1>{name}</h1>
+      <button onClick={() => addCount(count + 1)}>addCount</button>
+      <button onClick={() => setName('test')}>setName</button>
+    </>
+  )
 }
 
-export default App;
+export default App
